a0089c87d3be30c480ae04bdc9c12307ca900212
Zach-Adcock <zwa2014@gmail.com>
2022-03-27 22:20:28 -0700

    Updated resume and styling

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,7 @@ import { init } from "ityped";
 import { useEffect, useRef } from "react";
 import LinkedInLogo from "../../logos/LinkedInLogo.png";
 import GitHubLogo from "../../logos/GitHubLogo.png";
-import ResumePDF from "../../pdfs/ZachAdcockResume.pdf";
+import ResumePDF from "../../pdfs/Zach-Adcock-Resume-2022.pdf";
 
 
 
